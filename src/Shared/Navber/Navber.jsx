fda9a95d10feb0908537da90d6fb1347a9552a50
reshad21 <miakala7@gmail.com>
2022-12-27 09:33:17 +0600

    header and footer create

--- src/Shared/Navber/Navber.jsx
+++ src/Shared/Navber/Navber.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const Navber = () => {
+    const menuItems = <>
+        <li><a href='/'>Item 1</a></li>
+        <li tabIndex={0}>
+            <a href='/'>
+                Parent
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+            </a>
+            <ul className="p-2 bg-base-100">
+                <li><a href='/'>Submenu 1</a></li>
+                <li><a href='/'>Submenu 2</a></li>
+            </ul>
+        </li>
+        <li><a href='/'>Item 3</a></li>
+    </>
     return (
-        <div>
-            <h1>Navber </h1>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a href='/' className="btn btn-ghost normal-case text-xl">WebSpiderBd</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    {menuItems}
+                </ul>
+            </div>
         </div>
     );
 };
