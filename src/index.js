3912dbeadb100710a63b93fc4eae996cf1de36de
reshad21 <miakala7@gmail.com>
2022-12-30 10:14:35 +0600

    firebase authentication system add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
+import AuthProvider from './Pages/contexts/AuthProvider';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+        <App />
+      </AuthProvider>
   </React.StrictMode>
 );
 
