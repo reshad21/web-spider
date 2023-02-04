771d63fe28853d8ecb48af4f0c44d9b283797729
reshad21 <miakala7@gmail.com>
2023-02-04 11:12:54 +0600

    memeber component create

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Members from '../Members/Members';
 import PortfolioSection from '../PortfolioSection/PortfolioSection';
 import ServiceProvide from '../ServiceProvide/ServiceProvide';
 import Slider from '../Slider/Slider';
@@ -9,6 +10,7 @@ const Home = () => {
             <Slider></Slider>
             <ServiceProvide></ServiceProvide>
             <PortfolioSection></PortfolioSection>
+            <Members></Members>
             <h1>Services We Provide</h1>
             <p className='mb-36'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque tempora voluptatem magnam reprehenderit illum, in exercitationem nam quasi facere repudiandae, quam ipsam. Et veniam dolorem doloribus ad, atque sapiente repellendus velit blanditiis laboriosam praesentium ab ex amet corporis nulla iusto perspiciatis at mollitia voluptatum, delectus excepturi qui est doloremque! Animi quaerat esse velit repellendus amet sapiente fugit inventore dignissimos eos culpa odio dicta, commodi reprehenderit obcaecati ea, unde consequuntur labore. Ratione, nostrum? Beatae modi illo soluta quae reiciendis ad quaerat, hic molestiae, aliquid delectus sit earum debitis obcaecati laborum cupiditate sunt, recusandae nihil. Esse facilis sunt, ratione quisquam amet neque?</p>
             <p className='mb-36'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque tempora voluptatem magnam reprehenderit illum, in exercitationem nam quasi facere repudiandae, quam ipsam. Et veniam dolorem doloribus ad, atque sapiente repellendus velit blanditiis laboriosam praesentium ab ex amet corporis nulla iusto perspiciatis at mollitia voluptatum, delectus excepturi qui est doloremque! Animi quaerat esse velit repellendus amet sapiente fugit inventore dignissimos eos culpa odio dicta, commodi reprehenderit obcaecati ea, unde consequuntur labore. Ratione, nostrum? Beatae modi illo soluta quae reiciendis ad quaerat, hic molestiae, aliquid delectus sit earum debitis obcaecati laborum cupiditate sunt, recusandae nihil. Esse facilis sunt, ratione quisquam amet neque?</p>
