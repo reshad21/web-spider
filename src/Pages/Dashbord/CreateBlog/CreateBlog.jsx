33d77647baad3509e4e3a46348a3dfc4cc4a1bed
reshad21 <miakala7@gmail.com>
2023-02-08 14:05:26 +0600

    select option create

--- src/Pages/Dashbord/CreateBlog/CreateBlog.jsx
+++ src/Pages/Dashbord/CreateBlog/CreateBlog.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import designSelectBox from './CreateBlog.css';
 const CreateBlog = () => {
+    const animatedComponents = makeAnimated();
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ]
     return (
         <div className='mx-auto max-w-7xl px-3 py-5'>
             <h1 className='text-center font-bold text-4xl mb-2 text-[#444444e0]'>Create New Blog</h1>
             <div className="hero bg-base-200">
                 <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text font-bold">Title</span>
@@ -23,15 +31,21 @@ const CreateBlog = () => {
                             <label className="label">
                                 <p>
                                     <span className="label-text font-bold">Category</span>
-                                    <span className='text-sm'>(you can use multiple category for your blog)</span>
+                                    {/* <span className='text-sm'>(you can use multiple category for your blog)</span> */}
                                 </p>
                             </label>
-                            <input type="text" placeholder="" className="input input-bordered" />
+                            <Select
+                                closeMenuOnSelect={false}
+                                components={animatedComponents}
+                                isMulti
+                                options={options}
+                                className={designSelectBox}
+                            />
                         </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-success">Submit</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
