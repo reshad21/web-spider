f3e1cabcad56d4e59a1744f92a49a03191a2a5a8
reshad21 <miakala7@gmail.com>
2023-02-08 13:25:20 +0600

    create blog templeate

--- src/Pages/Dashbord/CreateBlog/CreateBlog.jsx
+++ src/Pages/Dashbord/CreateBlog/CreateBlog.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 
 const CreateBlog = () => {
     return (
-        <div>
-            <h1>CreateBlog</h1>
+        <div className='mx-auto max-w-7xl px-3 py-5'>
+            <h1 className='text-center font-bold text-4xl mb-2 text-[#444444e0]'>Create New Blog</h1>
+            <div className="hero bg-base-200">
+                <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-bold">Title</span>
+                            </label>
+                            <input type="text" placeholder="Title" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-bold">Description</span>
+                            </label>
+                            <textarea className="textarea textarea-bordered" placeholder="Bio" rows='5'></textarea>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <p>
+                                    <span className="label-text font-bold">Category</span>
+                                    <span className='text-sm'>(you can use multiple category for your blog)</span>
+                                </p>
+                            </label>
+                            <input type="text" placeholder="" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-success">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashbord/Dashbord.jsx
+++ src/Pages/Dashbord/Dashbord.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashbord = () => {
 
     return (
-        <div className='p-5 h-screen'>
+        <div className='p-5'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo earum in autem magni quia quam accusantium dignissimos reprehenderit veritatis amet minus odit, blanditiis quis suscipit maiores similique rem, tempora voluptatibus?Lorem ipsum dolor sit amet consectetur adipisicing elit. A asperiores quos laboriosam. Autem, repudiandae laborum nemo hic accusamus eaque, modi nostrum adipisci sapiente unde alias velit ullam dignissimos fugiat cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque minus dicta inventore quasi quam rem possimus quos blanditiis! Enim itaque deleniti autem laboriosam natus officiis mollitia dolorum doloribus nulla libero.
         </div>
     );
