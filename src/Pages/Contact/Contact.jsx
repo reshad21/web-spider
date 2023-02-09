a16903d41d8ba9596435f4a0f0076b4a421d2cc5
reshad21 <miakala7@gmail.com>
2023-02-09 13:06:24 +0600

    emailjs configuration done

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
+// import React from 'react';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
+import { toast } from 'react-hot-toast';
 import { FaFacebook, FaInstagram, FaLinkedin, FaRegComment, FaRegEnvelope, FaRegUser, FaTv, FaTwitter, FaViber, FaYoutube } from 'react-icons/fa';
 import { HiOutlineMail, HiOutlinePhone } from "react-icons/hi";
 import { Link } from 'react-router-dom';
 import CommonSection from '../../Shared/CommonSection/CommonSection';
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_ywvv4h3', 'template_3raknwd', form.current, 'gdw7FnmbhlbpG-f2o')
+            .then((response) => {
+                console.log('SUCCESS!', response.status);
+                toast.success('Your Message Successfully Send..!');
+                e.target.reset();
+            }, (err) => {
+                console.log('FAILED...', err);
+            });
+
+
+
+    }
     return (
         <div className='mx-auto max-w-7xl px-3'>
             <CommonSection className='pt-5 text-4xl text-success font-bold'>Send Us A Message</CommonSection>
             <div className="lg:flex md:flex gap-5  p-4 md:gap-7">
                 <div className='lg:w-[70%] md:w-[60%]'>
-                    <form className="">
+                    <form className="" ref={form} onSubmit={sendEmail}>
 
                         <div className="lg:flex md:flex justify-between gap-8 mb-5">
                             <div className="form-control relative w-full mb-5 lg:mb-0 md:mb-0">
                                 <label className="label absolute top-2">
                                     <FaRegUser />
                                 </label>
-                                <input type="text" placeholder="Name" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
+                                <input type="text" name='user_name' placeholder="Name" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
                             </div>
 
                             <div className="form-control relative w-full">
                                 <label className="label absolute top-2">
                                     <FaRegEnvelope />
                                 </label>
-                                <input type="email" placeholder="Email" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
+                                <input type="email" name='user_email' placeholder="Email" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
                             </div>
                         </div>
 
@@ -32,14 +51,14 @@ const Contact = () => {
                                 <label className="label absolute top-2">
                                     <FaViber />
                                 </label>
-                                <input type="text" placeholder="Phone" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
+                                <input type="text" name='user_phone' placeholder="Phone" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
                             </div>
 
                             <div className="form-control relative w-full">
                                 <label className="label absolute top-2">
                                     <FaTv />
                                 </label>
-                                <input type="text" placeholder="Website" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
+                                <input type="text" name='user_website' placeholder="Website" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" />
                             </div>
                         </div>
 
@@ -47,7 +66,7 @@ const Contact = () => {
                             <label className="label absolute top-2">
                                 <FaRegComment />
                             </label>
-                            <textarea className="textarea textarea-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" placeholder="Message"></textarea>
+                            <textarea name='message' className="textarea textarea-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0 bg-inherit" placeholder="Message"></textarea>
                         </div>
 
                         <div className="form-control mt-6">
--- src/Pages/Home/ReasonforChose/Quote/Quote.jsx
+++ src/Pages/Home/ReasonforChose/Quote/Quote.jsx
@@ -1,51 +1,70 @@
-import React from 'react';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
+import { toast } from 'react-hot-toast';
 import { FaRegComment, FaRegEnvelope, FaRegUser, FaTv, FaViber } from 'react-icons/fa';
-
 const Quote = () => {
+    // const serviceid = "service_ywvv4h3"
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_ywvv4h3', 'template_3raknwd', form.current, 'gdw7FnmbhlbpG-f2o')
+            .then((response) => {
+                console.log('SUCCESS!', response.status);
+                toast.success('Your Message Successfully Send..!');
+                e.target.reset();
+            }, (err) => {
+                console.log('FAILED...', err);
+            });
+
+
+
+    }
     return (
         <div className='lg:w-[40%] md:w-[40%]'>
             <div className="divider h-[3px] bg-success"></div>
             <h1 className='text-4xl font-bold pl-5 text-[#2f2f2fc9]'>Get a Free Quote</h1>
-            <div className="card-body">
+
+            <form className="card-body" ref={form} onSubmit={sendEmail}>
 
                 <div className="form-control relative">
                     <label className="label absolute top-2">
                         <FaRegUser />
                     </label>
-                    <input type="text" placeholder="Name" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
+                    <input type="text" name='user_name' placeholder="Name" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
                 </div>
 
                 <div className="form-control relative">
                     <label className="label absolute top-2">
                         <FaRegEnvelope />
                     </label>
-                    <input type="email" placeholder="Email" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
+                    <input type="email" name='user_email' placeholder="Email" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
                 </div>
 
                 <div className="form-control relative">
                     <label className="label absolute top-2">
                         <FaViber />
                     </label>
-                    <input type="text" placeholder="Phone" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
+                    <input type="text" name='user_phone' placeholder="Phone" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
                 </div>
 
                 <div className="form-control relative">
                     <label className="label absolute top-2">
                         <FaTv />
                     </label>
-                    <input type="text" placeholder="Website" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
+                    <input type="text" name='user_website' placeholder="Website" className="input input-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" />
                 </div>
 
                 <div className="form-control relative">
                     <label className="label absolute top-2">
                         <FaRegComment />
                     </label>
-                    <textarea className="textarea textarea-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" placeholder="Message"></textarea>
+                    <textarea name='message' className="textarea textarea-bordered pl-8 border-t-0 border-r-0 border-l-0 rounded-none outline-0 focus:outline-0" placeholder="Message"></textarea>
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-success btn-outline">Send Us</button>
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
